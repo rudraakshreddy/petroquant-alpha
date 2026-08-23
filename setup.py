2fbd8630d9263ebabdf9fc28cbe5cf88b452f350
rudraakshreddy <rudraakshreddy@users.noreply.github.com>
2026-08-23 20:47:13 +0530

    Add setup.py to package project

---
+++ setup.py
@@ -0,0 +1,22 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='crude-oil-crack-spreads',
+    version='1.0.0',
+    description='Mean Reversion Strategy for the 3:2:1 Crude Oil Crack Spread',
+    author='Rudraaksh Reddy',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'statsmodels',
+        'yfinance',
+        'matplotlib',
+        'seaborn',
+        'plotly',
+        'tqdm',
+        'tabulate',
+        'openpyxl'
+    ],
+)
